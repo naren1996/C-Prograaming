5c5eaffbb30bae1b2e11939cb98b21a3c21a8f16
naren1996 <30367065+naren1996@users.noreply.github.com>
2019-02-16 20:35:56 +0530

    Beautiful Ending Message

--- Scratch.C
+++ Scratch.C
@@ -5,4 +5,7 @@ void main()
 {
   printf("1.I am learning to use Github!\n");
   printf("2.Learning to Edit the current code and submit :\n");
+  
+  
+ printf("Thankyou the Code ends here only:")
 }
